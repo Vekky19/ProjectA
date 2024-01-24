5448034e3e119b553f52d36dd224bef8933d8979
vekky19 <coldspaceman19@gmail.com>
2024-01-23 22:22:50 -0500

    Toon Fantasy URP Import

---
+++ Assets/Scripts/InventoryUI.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    public GameObject GridSlots;
+    public GameObject ItemPrefab;
+
+    void AddInventoryItemUI(ItemInstance itemInstance)
+    {
+       
+    }
+}
